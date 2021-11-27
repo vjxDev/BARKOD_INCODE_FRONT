da66f3fcd1254579e07a91bbc4e3bb93055b6dad
vjxDev <60753340+vjxDev@users.noreply.github.com>
2021-11-27 12:59:25 +0100

    0.02 Responsive

---
+++ src/components/Konverzija.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+function Konverzija() {
+  const [konIz, setKonIz] = useState("RSD");
+  const [konU, setKonU] = useState("EUR");
+
+  return (
+    <div className="flex flex-col sm:flex-row gap-4 w-full items-end ">
+      <label className=" input-text">
+        <span class="text-gray-700">Količina:</span>
+        <input
+          type="text"
+          multiple
+          class="form-input mt-1 block w-full"
+          placeholder="1.00"
+        />
+      </label>
+      <label className=" input-text">
+        <span class="text-gray-700">Konverzija iz:</span>
+        <input
+          type="text"
+          multiple
+          class="form-input mt-1 block w-full"
+          placeholder="john@example.com"
+        />
+      </label>
+      <button className="grid items-center justify-center h-12 w-full sm:w-auto rounded-full  bg-primery text-white ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-8 h-8 mx-2"
+          viewBox="0 0 24 24"
+          fill="white"
+        >
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path d="M6.99 11L3 15l3.99 4v-3H14v-2H6.99v-3zM21 9l-3.99-4v3H10v2h7.01v3L21 9z" />
+        </svg>
+      </button>
+      <label className=" input-text">
+        <span class="text-gray-700">Konverzija u:</span>
+        <input
+          type="text"
+          multiple
+          class="form-input mt-1 block w-full"
+          placeholder="1 Eur"
+        />
+      </label>
+    </div>
+  );
+}
+
+export default Konverzija;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,44 +1,21 @@
+import Konverzija from "../components/Konverzija";
+
 function HomePage() {
   return (
     <div className="container mx-auto">
-      <div className="grid p-8 gap-4 bg-white border-primery border-2 rounded-3xl">
-        {/* forul */}
-        <div className="flex flex-row gap-2 w-full">
-          <label className="grid w-full">
-            <span class="text-gray-700">Količina:</span>
-            <input
-              type="text"
-              multiple
-              class="form-input mt-1 block w-full"
-              placeholder="1.00"
-            />
-          </label>
-          <label className="grid w-full">
-            <span class="text-gray-700">Konverzija iz:</span>
-            <input
-              type="text"
-              multiple
-              class="form-input mt-1 block w-full"
-              placeholder="john@example.com"
-            />
-          </label>
-          <button className="w-16 h-16 rounded-full bg-gray-600">Rotate</button>
-          <label className="grid w-full">
-            <span class="text-gray-700">Konverzija u:</span>
-            <input
-              type="text"
-              multiple
-              class="form-input mt-1 block w-full"
-              placeholder="john@example.com"
-            />
-          </label>
-        </div>
+      <div className="grid p-8 gap-8 bg-white border-primery border-2 rounded-xl">
+        {/* Konverzija */}
+        <Konverzija></Konverzija>
         {/* Rzultat */}
-        <div className="flex flex-row justify-between">
-          <div>
-            <span>1.00 Evro</span>
-            <br />
-            <span className=""> 117.563 Srpskih dinara</span>
+
+        <div className="flex gap-4 flex-col-reverse sm:flex-row justify-between sm:items-end  ">
+          <div className="grid gap-1">
+            <span className="text-lg">
+              1.00 Evro = <br />
+            </span>
+            <span className=" text-4xl text-primery font-semibold">
+              117.563 Srpskih dinara
+            </span>
           </div>
           <button className="btn">Izvrši konverzija </button>
         </div>
