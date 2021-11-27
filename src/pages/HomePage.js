80d43bb62e3b9449105f21ffa0424d9558174f41
Branimir <Branimir@DESKTOP-9EQC5MS>
2021-11-27 18:15:08 +0100

    Fixed title

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -59,7 +59,7 @@ function HomePage() {
 
   return (
     <div className="container grid mx-auto gap-8 relative p-10 sm:px-60 sm:mt-10 main-container">
-      <h1 className="webpage-title text-center">BARKODE EXCANGE</h1>
+      <h1 className="webpage-title text-center">BARKOD EXCHANGE</h1>
       <div className="bg-primery absolute h-[20wh] w-screen"></div>
       <div className="grid p-8 gap-8 bg-white border-primery border-2 rounded-xl">
         {/* Konverzija */}
